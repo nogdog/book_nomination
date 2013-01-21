7803bb144819ed9abe767443721fea71e8dad2dd
Charles Reace <charles.reace@comcast.net>
2013-01-20 23:39:20 -0500

    change default CI config main page naming

---
+++ application/logs/log-2013-01-21.php
@@ -0,0 +1,263 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-01-21 03:49:55 --> Config Class Initialized
+DEBUG - 2013-01-21 03:49:55 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:49:55 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:49:55 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:49:55 --> URI Class Initialized
+DEBUG - 2013-01-21 03:49:55 --> Router Class Initialized
+DEBUG - 2013-01-21 03:49:55 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 03:49:55 --> Output Class Initialized
+DEBUG - 2013-01-21 03:49:55 --> Security Class Initialized
+DEBUG - 2013-01-21 03:49:55 --> Input Class Initialized
+DEBUG - 2013-01-21 03:49:55 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:49:55 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:49:55 --> Language Class Initialized
+ERROR - 2013-01-21 03:49:55 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:50:20 --> Config Class Initialized
+DEBUG - 2013-01-21 03:50:20 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:50:20 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:50:20 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:50:20 --> URI Class Initialized
+DEBUG - 2013-01-21 03:50:20 --> Router Class Initialized
+DEBUG - 2013-01-21 03:50:20 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 03:50:20 --> Output Class Initialized
+DEBUG - 2013-01-21 03:50:20 --> Security Class Initialized
+DEBUG - 2013-01-21 03:50:20 --> Input Class Initialized
+DEBUG - 2013-01-21 03:50:20 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:50:20 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:50:20 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:50:20 --> Language Class Initialized
+ERROR - 2013-01-21 03:50:20 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:50:27 --> Config Class Initialized
+DEBUG - 2013-01-21 03:50:27 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:50:27 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:50:27 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:50:27 --> URI Class Initialized
+DEBUG - 2013-01-21 03:50:27 --> Router Class Initialized
+DEBUG - 2013-01-21 03:50:27 --> Output Class Initialized
+DEBUG - 2013-01-21 03:50:27 --> Security Class Initialized
+DEBUG - 2013-01-21 03:50:27 --> Input Class Initialized
+DEBUG - 2013-01-21 03:50:27 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:50:27 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:50:27 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:50:27 --> Language Class Initialized
+ERROR - 2013-01-21 03:50:27 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:51:22 --> Config Class Initialized
+DEBUG - 2013-01-21 03:51:22 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:51:22 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:51:22 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:51:22 --> URI Class Initialized
+DEBUG - 2013-01-21 03:51:22 --> Router Class Initialized
+DEBUG - 2013-01-21 03:51:22 --> Output Class Initialized
+DEBUG - 2013-01-21 03:51:22 --> Security Class Initialized
+DEBUG - 2013-01-21 03:51:22 --> Input Class Initialized
+DEBUG - 2013-01-21 03:51:22 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:51:22 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:51:22 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:51:22 --> Language Class Initialized
+ERROR - 2013-01-21 03:51:22 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:51:29 --> Config Class Initialized
+DEBUG - 2013-01-21 03:51:29 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:51:29 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:51:29 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:51:29 --> URI Class Initialized
+DEBUG - 2013-01-21 03:51:29 --> Router Class Initialized
+DEBUG - 2013-01-21 03:51:29 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 03:51:29 --> Output Class Initialized
+DEBUG - 2013-01-21 03:51:29 --> Security Class Initialized
+DEBUG - 2013-01-21 03:51:29 --> Input Class Initialized
+DEBUG - 2013-01-21 03:51:29 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:51:29 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:51:29 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:51:29 --> Language Class Initialized
+ERROR - 2013-01-21 03:51:29 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:51:38 --> Config Class Initialized
+DEBUG - 2013-01-21 03:51:38 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:51:38 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:51:38 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:51:38 --> URI Class Initialized
+DEBUG - 2013-01-21 03:51:38 --> Router Class Initialized
+DEBUG - 2013-01-21 03:51:38 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 03:51:38 --> Output Class Initialized
+DEBUG - 2013-01-21 03:51:38 --> Security Class Initialized
+DEBUG - 2013-01-21 03:51:38 --> Input Class Initialized
+DEBUG - 2013-01-21 03:51:38 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:51:38 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:51:38 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:51:38 --> Language Class Initialized
+ERROR - 2013-01-21 03:51:38 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:52:31 --> Config Class Initialized
+DEBUG - 2013-01-21 03:52:31 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:52:31 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:52:31 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:52:31 --> URI Class Initialized
+DEBUG - 2013-01-21 03:52:31 --> Router Class Initialized
+DEBUG - 2013-01-21 03:52:31 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 03:52:31 --> Output Class Initialized
+DEBUG - 2013-01-21 03:52:31 --> Security Class Initialized
+DEBUG - 2013-01-21 03:52:31 --> Input Class Initialized
+DEBUG - 2013-01-21 03:52:31 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:52:31 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:52:31 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:52:31 --> Language Class Initialized
+ERROR - 2013-01-21 03:52:31 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:52:48 --> Config Class Initialized
+DEBUG - 2013-01-21 03:52:48 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:52:48 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:52:48 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:52:48 --> URI Class Initialized
+DEBUG - 2013-01-21 03:52:48 --> Router Class Initialized
+DEBUG - 2013-01-21 03:52:48 --> Output Class Initialized
+DEBUG - 2013-01-21 03:52:48 --> Security Class Initialized
+DEBUG - 2013-01-21 03:52:48 --> Input Class Initialized
+DEBUG - 2013-01-21 03:52:48 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:52:48 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:52:48 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:52:48 --> Language Class Initialized
+ERROR - 2013-01-21 03:52:48 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:56:13 --> Config Class Initialized
+DEBUG - 2013-01-21 03:56:13 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:56:13 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:56:13 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:56:13 --> URI Class Initialized
+DEBUG - 2013-01-21 03:56:13 --> Router Class Initialized
+DEBUG - 2013-01-21 03:56:13 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 03:56:13 --> Output Class Initialized
+DEBUG - 2013-01-21 03:56:13 --> Security Class Initialized
+DEBUG - 2013-01-21 03:56:13 --> Input Class Initialized
+DEBUG - 2013-01-21 03:56:13 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:56:13 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:56:13 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:56:13 --> Language Class Initialized
+ERROR - 2013-01-21 03:56:13 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:56:25 --> Config Class Initialized
+DEBUG - 2013-01-21 03:56:25 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:56:25 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:56:25 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:56:25 --> URI Class Initialized
+DEBUG - 2013-01-21 03:56:25 --> Router Class Initialized
+DEBUG - 2013-01-21 03:56:25 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 03:56:25 --> Output Class Initialized
+DEBUG - 2013-01-21 03:56:25 --> Security Class Initialized
+DEBUG - 2013-01-21 03:56:25 --> Input Class Initialized
+DEBUG - 2013-01-21 03:56:25 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:56:25 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:56:25 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:56:25 --> Language Class Initialized
+ERROR - 2013-01-21 03:56:25 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:56:31 --> Config Class Initialized
+DEBUG - 2013-01-21 03:56:31 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:56:31 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:56:31 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:56:31 --> URI Class Initialized
+DEBUG - 2013-01-21 03:56:31 --> Router Class Initialized
+DEBUG - 2013-01-21 03:56:31 --> Output Class Initialized
+DEBUG - 2013-01-21 03:56:31 --> Security Class Initialized
+DEBUG - 2013-01-21 03:56:31 --> Input Class Initialized
+DEBUG - 2013-01-21 03:56:31 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:56:31 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:56:31 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:56:31 --> Language Class Initialized
+ERROR - 2013-01-21 03:56:31 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 03:59:49 --> Config Class Initialized
+DEBUG - 2013-01-21 03:59:49 --> Hooks Class Initialized
+DEBUG - 2013-01-21 03:59:49 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 03:59:49 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 03:59:49 --> URI Class Initialized
+DEBUG - 2013-01-21 03:59:49 --> Router Class Initialized
+DEBUG - 2013-01-21 03:59:49 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 03:59:49 --> Output Class Initialized
+DEBUG - 2013-01-21 03:59:49 --> Security Class Initialized
+DEBUG - 2013-01-21 03:59:49 --> Input Class Initialized
+DEBUG - 2013-01-21 03:59:49 --> XSS Filtering completed
+DEBUG - 2013-01-21 03:59:49 --> CRSF cookie Set
+DEBUG - 2013-01-21 03:59:49 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 03:59:49 --> Language Class Initialized
+ERROR - 2013-01-21 03:59:49 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 04:02:22 --> Config Class Initialized
+DEBUG - 2013-01-21 04:02:22 --> Hooks Class Initialized
+DEBUG - 2013-01-21 04:02:22 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 04:02:22 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 04:02:22 --> URI Class Initialized
+DEBUG - 2013-01-21 04:02:22 --> Router Class Initialized
+DEBUG - 2013-01-21 04:02:22 --> Output Class Initialized
+DEBUG - 2013-01-21 04:02:22 --> Security Class Initialized
+DEBUG - 2013-01-21 04:02:22 --> Input Class Initialized
+DEBUG - 2013-01-21 04:02:22 --> XSS Filtering completed
+DEBUG - 2013-01-21 04:02:22 --> CRSF cookie Set
+DEBUG - 2013-01-21 04:02:22 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 04:02:22 --> Language Class Initialized
+ERROR - 2013-01-21 04:02:22 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 04:03:30 --> Config Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> Hooks Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 04:03:30 --> URI Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> Router Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 04:03:30 --> Output Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> Security Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> Input Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> XSS Filtering completed
+DEBUG - 2013-01-21 04:03:30 --> CRSF cookie Set
+DEBUG - 2013-01-21 04:03:30 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 04:03:30 --> Language Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> Loader Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> Controller Class Initialized
+DEBUG - 2013-01-21 04:03:30 --> File loaded: application/views/welcome_message.php
+DEBUG - 2013-01-21 04:03:30 --> Final output sent to browser
+DEBUG - 2013-01-21 04:03:30 --> Total execution time: 0.0477
+DEBUG - 2013-01-21 04:06:55 --> Config Class Initialized
+DEBUG - 2013-01-21 04:06:55 --> Hooks Class Initialized
+DEBUG - 2013-01-21 04:06:55 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 04:06:55 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 04:06:55 --> URI Class Initialized
+DEBUG - 2013-01-21 04:06:55 --> Router Class Initialized
+DEBUG - 2013-01-21 04:06:55 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 04:06:55 --> Output Class Initialized
+DEBUG - 2013-01-21 04:06:55 --> Security Class Initialized
+DEBUG - 2013-01-21 04:06:55 --> Input Class Initialized
+DEBUG - 2013-01-21 04:06:55 --> XSS Filtering completed
+DEBUG - 2013-01-21 04:06:55 --> CRSF cookie Set
+DEBUG - 2013-01-21 04:06:55 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 04:06:55 --> Language Class Initialized
+ERROR - 2013-01-21 04:06:55 --> 404 Page Not Found --> main/index
+DEBUG - 2013-01-21 04:08:13 --> Config Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> Hooks Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 04:08:13 --> URI Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> Router Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 04:08:13 --> Output Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> Security Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> Input Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> XSS Filtering completed
+DEBUG - 2013-01-21 04:08:13 --> CRSF cookie Set
+DEBUG - 2013-01-21 04:08:13 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 04:08:13 --> Language Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> Loader Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> Controller Class Initialized
+DEBUG - 2013-01-21 04:08:13 --> File loaded: application/views/main_page.php
+DEBUG - 2013-01-21 04:08:13 --> Final output sent to browser
+DEBUG - 2013-01-21 04:08:13 --> Total execution time: 0.0285
+DEBUG - 2013-01-21 04:08:56 --> Config Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> Hooks Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> Utf8 Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> UTF-8 Support Enabled
+DEBUG - 2013-01-21 04:08:56 --> URI Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> Router Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> No URI present. Default controller set.
+DEBUG - 2013-01-21 04:08:56 --> Output Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> Security Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> Input Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> XSS Filtering completed
+DEBUG - 2013-01-21 04:08:56 --> CRSF cookie Set
+DEBUG - 2013-01-21 04:08:56 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-01-21 04:08:56 --> Language Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> Loader Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> Controller Class Initialized
+DEBUG - 2013-01-21 04:08:56 --> File loaded: application/views/main_page.php
+DEBUG - 2013-01-21 04:08:56 --> Final output sent to browser
+DEBUG - 2013-01-21 04:08:56 --> Total execution time: 0.0339
---
+++ application/views/main_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+
+<h1>Book Nomination Tool</h1>
+<p>Under construction...</p>
+
+</body>
+</html>
